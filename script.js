0ee1727be74e59de3acb09fbc778483ce7b98b9d
Muhammad Yusran Yusran <>
2022-06-11 16:17:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Memasukkan angka genap ke dalam array menggunakan for dan if
+let evenNumber = [];
+for(length = 1; length <= 100; length++){
+  if(length % 2 == 0){
+    evenNumber.push(length);
+  }
+}
+console.log(evenNumber);
+console.log(evenNumber.length);
+    
+
+// let evenNumber = [1];
+// let genap = [];
+// for(evenNumber.length; evenNumber.length <= 100; evenNumber.length++){
+//   // console.log(evenNumber.push(evenNumber.length));
+//   genap.push(evenNumber.length);
+// }
+// console.log(genap);
+// console.log(evenNumber.length);  
